8c9f1dc250a6dcb1cb6eb5afd153d70e2789a535
aleDonatto <ale_donatto@yahoo.com.mx>
2023-01-26 01:59:29 -0600

    [feat] add options menu

--- src/components/bev/Modulos.vue
+++ src/components/bev/Modulos.vue
@@ -17,6 +17,18 @@
                                         <p class="px-5 py-2 font-weight-bold font-avenir font-size-28 text-center">Modo 1</p>
                                     </div>
                                 </template>
+
+                                <v-card-text>
+                                    <v-img :src="vehicles" max-height="80" cover></v-img>
+
+                                    <div class="bg-yellow-p mt-5">
+                                        <p class="px-1 py-2">
+                                            Conexión de baja corriente (AC) por medio de un enchufe normal.
+                                            <br><br>
+                                            Se utiliza principalmente para vehículos ligeros.
+                                        </p>
+                                    </div>
+                                </v-card-text>
                             </v-card>
                         </v-col>
                         <v-col cols="3">
@@ -26,6 +38,22 @@
                                         <p class="px-5 py-2 font-weight-bold font-avenir font-size-28 text-center">Modo 2</p>
                                     </div>
                                 </template>
+
+                                <v-card-text>
+                                    <v-img :src="control" max-height="80" cover></v-img>
+
+                                    <div class="bg-yellow-p mt-5">
+                                        <p class="px-1 py-2">
+                                            Se utiliza un cable que contiene una unidad de control y se conecta a un enchufe ordinario (CA).
+                                            <br><br>
+                                            Puede conectarse a 110V o a 220V, y con ello aumentar la velocidad de carga.
+                                            <br><br>
+                                            <span class="font-weight-bold">
+                                            El Cable de Carga que se incluye con cada vehículo nuevo SEV es considerado modo 2 de carga.
+                                            </span>
+                                        </p>
+                                    </div>
+                                </v-card-text>
                             </v-card>
                         </v-col>
                         <v-col cols="3">
@@ -35,6 +63,22 @@
                                         <p class="px-5 py-2 font-weight-bold font-avenir font-size-28 text-center">Modo 3</p>
                                     </div>
                                 </template>
+
+                                <v-card-text>
+                                    <v-img :src="comunicacion" max-height="80" cover></v-img>
+
+                                    <div class="bg-yellow-p mt-5">
+                                        <p class="px-1 py-2">
+                                            Cargador de pared conectado a la red eléctrica (CA) a 220V.
+                                            <br><br>
+                                            La unidad de control se encuentra instalada en el cargador de pared o Wallbox.
+                                            <br><br>
+                                            <span class="font-weight-bold">
+                                                Independientemente de la potencia que pueda suministrar el cargador, el vehículo podrá aprovechar la potencia de acuerdo a sus capacidades.
+                                            </span>
+                                        </p>
+                                    </div>
+                                </v-card-text>
                             </v-card>
                         </v-col>
                         <v-col cols="3">
@@ -44,6 +88,22 @@
                                         <p class="px-5 py-2 font-weight-bold font-avenir font-size-28 text-center">Modo 4</p>
                                     </div>
                                 </template>
+                                <v-card-text>
+                                    <v-img :src="cable" max-height="80" cover></v-img>
+
+                                    <div class="bg-yellow-p mt-5">
+                                        <p class="px-1 py-2">
+                                            Carga rápida de Corriente Directa (CD).
+                                            <br><br>
+                                            Se utiliza sólo para vehículos 100% eléctricos.
+                                            <br><br>
+                                            <span class="font-weight-bold">
+                                            La potencia de estos cargadores puede llegar a superar los 300 kW, 
+                                            sin embargo no todos los autos lo soportan.
+                                            </span>
+                                        </p>
+                                    </div>
+                                </v-card-text>
                             </v-card>
                         </v-col>
                     </v-row>
@@ -56,6 +116,10 @@
 <script setup>
 import ContentTemplate from '../templates/ContentTemplate.vue';
 import user from '@/assets/evolucion/user.png'
+import cable from '@/assets/cargando/cable.png'
+import comunicacion from '@/assets/cargando/comunicacion.png'
+import control from '@/assets/cargando/control.png'
+import vehicles from '@/assets/cargando/vehicles.png'
 </script>
 
 <style scoped>
