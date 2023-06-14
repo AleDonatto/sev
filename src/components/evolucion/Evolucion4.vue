b6d7e2478fb74a3c642fd9f743112698b97ba9bb
aleDonatto <ale_donatto@yahoo.com.mx>
2023-06-14 12:20:25 -0600

    [feat] fixes images on module evolucion

--- src/components/evolucion/Evolucion4.vue
+++ src/components/evolucion/Evolucion4.vue
@@ -22,21 +22,21 @@
                         </v-col>
                     </v-row>
 
-                    <v-row no-gutters class="mt-10 mb-10 animate__animated animate__backInLeft">
+                    <v-row no-gutters class="animate__animated animate__backInLeft" :class="{'mt-10 mb-10': windowHeight> 800 }">
                         <v-col cols="6" class="">
-                            <v-img src="@/assets/evolucion/sev1024.png" contain :max-height="windowHeight > 800 ? '170' : '150'"></v-img>
+                            <v-img src="@/assets/evolucion/sev1024.png" contain :max-height="windowHeight > 800 ? '170' : '130'"></v-img>
                         </v-col>
                         <v-col cols="6">
-                            <v-img src="@/assets/evolucion/sev1160.png" contain :max-height="windowHeight > 800 ? '170' : '150'"></v-img>
+                            <v-img src="@/assets/evolucion/sev1160.png" contain :max-height="windowHeight > 800 ? '170' : '130'"></v-img>
                         </v-col>
                         <v-col cols="4" class="mt-5">
-                            <v-img src="@/assets/evolucion/camioneta.png" contain :max-height="windowHeight > 800 ? '170' : '150'"></v-img>
+                            <v-img src="@/assets/evolucion/camioneta.png" contain :max-height="windowHeight > 800 ? '170' : '130'"></v-img>
                         </v-col>
                         <v-col cols="4" class="mt-5">
-                            <v-img src="@/assets/evolucion/sedan.png" contain :max-height="windowHeight > 800 ? '170' : '150'"></v-img>
+                            <v-img src="@/assets/evolucion/sedan.png" contain :max-height="windowHeight > 800 ? '170' : '130'"></v-img>
                         </v-col>
                         <v-col cols="4" class="mt-5">
-                            <v-img src="@/assets/evolucion/trailer.png" contain :max-height="windowHeight > 800 ? '170' : '150'"></v-img>
+                            <v-img src="@/assets/evolucion/trailer.png" contain :max-height="windowHeight > 800 ? '170' : '130'"></v-img>
                         </v-col>
                     </v-row>
 
